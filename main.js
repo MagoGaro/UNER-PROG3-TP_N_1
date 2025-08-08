bef8b29be8f284c65d4fd1564d695489dca6b514
MagoGaro <gabriel.magogaro@gmail.com>
2025-08-08 11:49:26 -0300

    se crea el archivo js

---
+++ main.js
@@ -0,0 +1,8 @@
+// Declaracióin array
+let productos = [
+  { id: 1, nombre: "Pantalon Jean", precio: 75000, stock: 100 },
+  { id: 2, nombre: "Camisa Bordo", precio: 150000, stock: 5 },
+  { id: 3, nombre: "Remera Blanca", precio: 50000, stock: 20 },
+  { id: 4, nombre: "Camisa Negra", precio: 150000, stock: 1 },
+  { id: 5, nombre: "Pantalon Vestir", precio: 170000, stock: 3 }
+];
